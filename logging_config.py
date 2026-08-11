d78695dd7ea53171c0f18d721444a8b9c7f6473e
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2026-08-11 15:16:27 +0300

    Create logger config file

---
+++ logging_config.py
@@ -0,0 +1,29 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+LOG_FILE = "vision_pi.log"
+
+
+def setup_logging():
+    logger = logging.getLogger()
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
+    file_handler = RotatingFileHandler(
+        LOG_FILE,
+        maxBytes=5 * 1024 * 1024,
+        backupCount=3,
+    )
+
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
