ac752b2675187b9fa933a167f68b24ca0ceebdbe
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2026-08-12 14:52:44 +0300

    Add more AI parameters

--- config.py
+++ config.py
@@ -21,3 +21,23 @@ MOTION_THRESHOLD = 25
 MIN_MOTION_AREA = 5000
 MOTION_CHECK_INTERVAL = 0.1
 MOTION_COOLDOWN = 10
+
+MOTION_AI_ENABLED = True
+
+MOTION_AI_COOLDOWN = 30
+
+MOTION_AI_PROMPT = (
+    "You are analyzing a security camera image.\n\n"
+    "Determine whether the image contains something that "
+    "should trigger a security notification.\n\n"
+    "Important events include:\n"
+    "- a person\n"
+    "- an animal\n"
+    "- an unfamiliar or unusual object\n"
+    "- something that appears to have changed significantly\n\n"
+    "Do not trigger an alert for minor lighting changes, "
+    "shadows, reflections, or insignificant movements.\n\n"
+    "Respond with exactly two lines:\n"
+    "ALERT: YES or NO\n"
+    "DESCRIPTION: <short description>"
+)
